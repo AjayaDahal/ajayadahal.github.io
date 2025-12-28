8d51773d04a28809e49e232c8371bdeef94088a2
Ajaya Dahal <ajayadahal1000@gmail.com>
2025-12-28 02:50:09 -0600

    Fix more GUI stuffs

--- js/main.js
+++ js/main.js
@@ -382,25 +382,40 @@ window.onload = function() {
   //select all the anchors in the navbar one after another
   $navbara.each(function(){
    // and adds them in the sections variable
-    sections.push($($(this).attr('href')));
-    
-  })
+    var href = $(this).attr('href');
+    // Handle both direct IDs (like #page-1) and nested section IDs (like #featured-amd)
+    var $target = $(href);
+    if ($target.length > 0) {
+      sections.push($target);
+    }
+  });
+  
+  // Trigger initial scrollspy check on page load
+  setTimeout(function() {
+    $(window).trigger('scroll');
+  }, 100);
+  
   $(window).scroll(function(e){
-    // scrollTop retains the value of the scroll top with the reference at the middle of the page
-    var scrollTop = $(this).scrollTop() + ($(window).height()/2);
+    // scrollTop retains the value of the scroll top with the reference at the 1/3 point of the page
+    var scrollTop = $(this).scrollTop() + ($(window).height()/3);
     //cycle through the values in sections array
+    var scrolled_id = null;
     for (var i in sections) {
       var section = sections[i];
       //if scrollTop variable is bigger than the top offset of a section in the sections array then 
       if (scrollTop > section.offset().top){
-        var scrolled_id = section.attr('id');
+        scrolled_id = section.attr('id');
       }
     }
     if (scrolled_id !== id) {
       id = scrolled_id;
       $($navbara).removeClass('current');
-      $('#navi a[href="#' + id + '"]').addClass('current'); 
+      if (id) {
+        $('#navi a[href="#' + id + '"]').addClass('current');
+      }
     }
+    // Always trigger the event to update submenu display
+    window.dispatchEvent(new Event('scrollspy-update'));
   })
 })(jQuery);
 
